e7efa3588d780ff609d03b49537c8a250b1b4147
ilushareg <ilushareg@g>
2022-01-07 23:07:24 +0300

    Various Assignment tests

--- TestReflection.cs
+++ TestReflection.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Reflection;
 
 namespace Chapter1App
 {
@@ -8,7 +7,7 @@ namespace Chapter1App
         void Set(object val);
     }
 
-    public class WrapperInt:IWrapper
+    public class WrapperInt : IWrapper
     {
         private int? pointer;
         public WrapperInt(ref int val)
@@ -21,25 +20,53 @@ namespace Chapter1App
             pointer = (int)val;
         }
     }
+    public class Assigner
+    {
+        //Func<int> assigner;
+        public Action<int> assignerFunc;
+        public Assigner(Action<int> f)
+        {
+            assignerFunc = f;
+        }
+        public void Set(int val)
+        {
+            assignerFunc.Invoke((val));
+        }
+    }
     public class TestReflection
     {
         public int testint = new int();
         static public void Foo()
         {
-            var instance = new TestReflection();
-            instance.testint = 10;
-            var info = instance.GetType().GetField("testint");
-            Console.WriteLine(instance.testint);
-            instance.testint
-            Console.WriteLine(instance.testint);
+            Assigner a;
+            {
+                var instance = new TestReflection();
+                instance.testint = 10;
+                var info = instance.GetType().GetField("testint");
+                Console.WriteLine(instance.testint);
+                int? tmp = 25;
+                instance.testint = (int)(object)(tmp);
+                Console.WriteLine(instance.testint);
+                TestAssign(ref instance.testint);
+                Console.WriteLine(instance.testint);
 
-            
-            object testobj = 12;
 
-            IWrapper wrapper = new WrapperInt(ref instance.testint);
-            wrapper.Set(testobj);            
+                object testobj = 12;
 
-            Console.WriteLine(instance.testint);
+                IWrapper wrapper = new WrapperInt(ref instance.testint);
+                wrapper.Set(testobj);
+
+                Console.WriteLine(instance.testint);
+                a = new Assigner((x) => { instance.testint = x; });
+            }
+            a.Set(101);
+            //Console.WriteLine(instance.testint);
+        }
+        static public void TestAssign(ref int val)
+        {
+            int? tmp = 99;
+            val = (int)(object)(tmp);
+
         }
     }
 }
